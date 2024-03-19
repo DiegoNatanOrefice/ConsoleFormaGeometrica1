80d5c9e657e519e5f348c2515cd2bd8d52d16841
Usuario <Usuario@CPD02-M19>
2024-03-19 11:00:34 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleFormaGeometrica1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a forma geométrica:(Quadrado, Retângulo, Trapézio, Losango): ");
+            string forma = (Console.ReadLine());
+
+            switch (forma)
+            {
+                case "Quadrado":
+                    Console.Write("Você escolheu: Quadrado");
+                    Console.ReadKey();
+                    break;
+
+                case "Retângulo":
+                    Console.Write("Você escolheu: Retângulo");
+                    Console.ReadKey();
+                    break;
+
+                case "Trapézio":
+                    Console.Write("Você escolheu: Trapézio");
+                    Console.ReadKey();
+                    break;
+
+                case "Losango":
+                    Console.Write("Você escolheu: Losango");
+                    Console.ReadKey();
+                    break;
+
+                default:
+                    Console.Write("Opção inválida");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
